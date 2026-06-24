fa9389795853cddb9dfead72e4892c8590e0222c
Muhammad Nabiluddin <muhammadnabiludin1106@gmail.com>
2026-06-24 10:23:38 +0700

    feat: loading img dan broken img

--- app/src/main/java/com/d3if4802/buslog2/ui/HomeScreen.kt
+++ app/src/main/java/com/d3if4802/buslog2/ui/HomeScreen.kt
@@ -15,10 +15,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.d3if4802.buslog2.R
 import com.d3if4802.buslog2.network.ApiState
 import com.d3if4802.buslog2.model.BusLog
@@ -81,9 +84,12 @@ fun HomeScreen(
                             color = MaterialTheme.colorScheme.error,
                             textAlign = TextAlign.Center
                         )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick = { viewModel.getLogs(userEmail) }) {
-                            Text("Coba Lagi")
+                        Button(
+                            onClick = { viewModel.getLogs(userEmail) },
+                            modifier = Modifier.padding(top = 16.dp),
+                            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+                        ) {
+                            Text(text = "Coba lagi")
                         }
                     }
                 }
@@ -142,9 +148,14 @@ fun LogCardItem(
         Column(modifier = Modifier.padding(16.dp)) {
             if (!log.imageUrl.isNullOrEmpty()) {
                 AsyncImage(
-                    model = log.imageUrl,
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(log.imageUrl)
+                        .crossfade(true)
+                        .build(),
                     contentDescription = stringResource(R.string.cd_bus_photo),
                     contentScale = ContentScale.Crop,
+                    placeholder = painterResource(id = R.drawable.loading_img),
+                    error = painterResource(id = R.drawable.broken_img),
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(200.dp)
